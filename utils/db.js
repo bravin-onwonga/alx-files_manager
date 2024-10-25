a964821a0d4211a7015cf7f24145cf22261c4d07
bravin-onwonga <onbravin@gmail.com>
2024-10-25 21:53:19 +0300

    chore:Add new user

--- utils/db.js
+++ utils/db.js
@@ -7,12 +7,22 @@ class DBClient {
     const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}/${database}`;
     this.client = new mongodb.MongoClient(url, { useUnifiedTopology: true });
-    this.client.connect().then(() => {
-      this.client.db().createCollection('users');
-      this.client.db().createCollection('files');
+    this.connect().then(() => {
+      console.log('Connection Successful');
     }).catch((err) => {
       console.error('MongoDB connection error:', err);
     });
+  }
+
+  async connect() {
+    try {
+      await this.client.connect();
+      await this.client.db().createCollection('users');
+      await this.client.db().createCollection('files');
+    } catch (error) {
+      console.error('Error creating collections:', error);
+      throw error;
+    }
   }
 
   isAlive() {
@@ -28,6 +38,6 @@ class DBClient {
   }
 }
 
-const dbClient = new DBClient();
+export const dbClient = new DBClient();
 
 export default dbClient;
