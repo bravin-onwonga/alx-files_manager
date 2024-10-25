36468265afeee3abbf572768f3e08e5a08c3d47c
bravin-onwonga <onbravin@gmail.com>
2024-10-25 14:53:14 +0300

    First try

---
+++ utils/db.js
@@ -0,0 +1,30 @@
+import mongodb from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
+    this.client = new mongodb.MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect();
+    this.client.db().createCollection('users');
+    this.client.db().createCollection('files');
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
